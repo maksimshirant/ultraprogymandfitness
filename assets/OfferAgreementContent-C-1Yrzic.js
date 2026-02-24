7a4f7f183d44b2c5e7b288353532d7e4279c43b7
maksimshirant <maksimshirant@users.noreply.github.com>
2026-02-24 10:19:05 +0000

    deploy: 57f4ef6cceb497480903faa83eae7057e3406422

--- assets/OfferAgreementContent-C-1Yrzic.js
+++ assets/OfferAgreementContent-C-1Yrzic.js
@@ -1,4 +1,4 @@
-import{j as t,B as a,a as e}from"./index-ItSbtrO6.js";const i="Договор офферты",l=`ДОГОВОР ПУБЛИЧНОЙ ОФЕРТЫ
+import{j as t,B as a,a as e}from"./index-B82WV0wq.js";const i="Договор офферты",l=`ДОГОВОР ПУБЛИЧНОЙ ОФЕРТЫ
 
 на оказание услуг фитнес-клуба «УЛЬТРА ПРО»
 
